e974cf0b39d7e34f2ec2456447b8f0070f9b3f62
ParthibanR <parthibanvijay467@gmail.com>
2026-03-02 12:03:17 +0530

    MODIFIED: App files modified. ADDED: Tailwind margin and padding examples are added.

--- tailwind-design-system/src/App.jsx
+++ tailwind-design-system/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TailWindColors from './tailwind-colors/tailwindColors'
+import TailWindMarginPadding from './tailwind-margin-padding/TailWindMarginPadding'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <TailWindColors/>
+      <TailWindMarginPadding/>
     </>
   )
 }
---
+++ tailwind-design-system/src/tailwind-margin-padding/TailWindMarginPadding.jsx
@@ -0,0 +1,27 @@
+function TailWindMarginPadding() {
+    return (
+      <>
+        <div className="p-4 m-2">
+          <p className="m-1 bg-gray-200">Hello World, TailWind CSS (m-1)</p>
+          <p className="ml-3 bg-gray-200">Hello World, TailWind CSS (ml-3)</p>
+          <p className="mr-4 bg-gray-200">Hello World, TailWind CSS (mr-4)</p>
+          <p className="mt-5 bg-gray-200">Hello World, TailWind CSS (mt-5)</p>
+          <p className="mb-3 bg-gray-200">Hello World, TailWind CSS (mb-3)</p>
+          <p className="mx-4 bg-gray-200">Hello World, TailWind CSS (mx-4)</p>
+          <p className="my-5 bg-gray-200">Hello World, TailWind CSS (my-5)</p>
+        </div>
+  
+        <div className="p-4 m-2">
+          <p className="p-1 bg-gray-200">Hello World, TailWind CSS (p-1)</p>
+          <p className="pl-3 bg-gray-200">Hello World, TailWind CSS (pl-3)</p>
+          <p className="pr-4 bg-gray-200">Hello World, TailWind CSS (pr-4)</p>
+          <p className="pt-5 bg-gray-200">Hello World, TailWind CSS (pt-5)</p>
+          <p className="pb-3 bg-gray-200">Hello World, TailWind CSS (pb-3)</p>
+          <p className="px-4 bg-gray-200">Hello World, TailWind CSS (px-4)</p>
+          <p className="py-5 bg-gray-200">Hello World, TailWind CSS (py-5)</p>
+        </div>
+      </>
+    );
+  }
+  
+  export default TailWindMarginPadding;
